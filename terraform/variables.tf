d359b5314f1243bb21e1d7742459dda8d4141744
AjinkyaP-09 <pameajinkya09@gmail.com>
2026-04-12 18:11:33 +0000

    Change region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,7 +15,7 @@
 variable "aws_region" {
   description = "AWS region to deploy all resources"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 # ─────────────────────────────────────────────────────────────────────────────
